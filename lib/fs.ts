248e4698b9bc8566f869995d05e6a96607505ac5
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-04-07 16:23:22 -0700

    Update comment to be true

--- lib/fs.ts
+++ lib/fs.ts
@@ -2,7 +2,7 @@ import { writeFileSync } from 'fs'
 import { join } from 'path'
 
 /**
- * Creates a file, but doesn't directly import `fs`. It imports `jsonfile`, which imports `fs`.
+ * Uses `fs` to create a file.
  */
 const createFile = (): void => {
   writeFileSync(join(process.cwd(), 'file.txt'), 'Hi')
