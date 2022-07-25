fc04d35bc89a0303f0ab37c86d7f919e4b017412
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-07-25 11:10:39 -0700

    Try doing snapshot resolver

---
+++ snapshotResolver.js
@@ -0,0 +1,16 @@
+const { join, relative } = require('path/posix')
+
+module.exports = {
+  // resolves from test to snapshot path
+  resolveSnapshotPath: (testPath, snapshotExtension) => {
+   return join('lib', relative('dist', testPath.replace('__tests__', '__snapshots__'))) + snapshotExtension
+  },
+
+  // resolves from snapshot to test path
+  resolveTestPath: (snapshotFilePath, snapshotExtension) => {
+    return join('dist', relative('lib', snapshotFilePath.replace('__snapshots__', '__tests__').slice(0, -snapshotExtension.length)))
+  },
+
+  // Example test path, used for preflight consistency check of the implementation above
+  testPathForConsistencyCheck: 'dist/__tests__/example.test.js',
+}
